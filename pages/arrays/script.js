e8eaf2c30e6add17345da3c84944f28c7ffb363a
lucastak <contato.lucas.santos@hotmail.com>
2022-12-18 18:41:32 -0300

    feat: adiciona estilizacao mobile e header e finaliza de estruturar card no js

--- pages/arrays/script.js
+++ pages/arrays/script.js
@@ -6,29 +6,65 @@ function getMovies(url) {
 }
 
 function criarElemento(filme) {
+  console.log("filme", filme);
   const card = document.createElement("div");
+  card.classList.add("card__container");
   let contador = 0;
   let rating = 0;
   card.innerHTML = `
-        <div class="card-movies__container">
-            <h2>${filme.titulo}</h2>
-            ${filme.opinioes
-              .map((element) => {
-                contador++;
-                rating += element.rating;
-                rating;
-              })
-              .join("")}
+            <div class="card__top">
+              <img class="card__top-image" alt="Banner do filme/serie" src="${
+                filme.figura
+              }"/>
 
-            <span class="card-movies__stars">${
-              rating / contador
-            } estrelas</span>
+              <div class="card__top-middle">
+                <h2>${filme.titulo}</h2>
+                <div class="card__top-generos">
+                  ${filme.generos
+                    .map((element) => {
+                      return `<span>${element}</span>, `;
+                    })
+                    .join("")}
+                </div>
+                <div class="card__top-elenco">
+                  <strong>Elenco: </strong>
+                  ${filme.elenco
+                    .map((element) => {
+                      return `<span>${element}</span>, `;
+                    })
+                    .join("")}
+                </div>
+                
+              </div>
+
+              <div class="card__top-ratings">
+                <span class="card__top-classificacao--${filme.classificacao}">${
+    filme.classificacao == 0 ? "Livre" : `${filme.classificacao}`
+  }</span>
+                
+                ${filme.opinioes
+                  .map((element) => {
+                    contador++;
+                    rating += element.rating;
+                  })
+                  .join("")}
+
+                <span class="card-top__stars">${
+                  rating / contador < 5
+                    ? `<img alt="estrela de raking" src="./star-yellow.svg"/> <img alt="estrela de raking" 
+                    src="./star-yellow.svg"/> <img alt="estrela de raking" src="./star-yellow.svg"/> 
+                    <img alt="estrela de raking" src="./star-yellow.svg"/> <img alt="estrela de raking" src="./star-grey.svg"/>`
+                    : `<img alt="estrela de raking" src="./star-yellow.svg"/> <img alt="estrela de raking" src="./star-yellow.svg"/> 
+                    <img alt="estrela de raking" src="./star-yellow.svg"/> <img alt="estrela de raking" 
+                    src="./star-yellow.svg"/> <img alt="estrela de raking" src="./star-yellow.svg"/>`
+                }
+                </span>
+              </div>
+            </div> 
             
-            <br/>
-            <span>${
-              filme.classificacao == 0 ? "Livre" : `${filme.classificacao}`
-            }</span>
-        </div>
+            <div class="card__resumo">
+                <span>${filme.resumo}</span>
+            </div>
     `;
 
   return card;
