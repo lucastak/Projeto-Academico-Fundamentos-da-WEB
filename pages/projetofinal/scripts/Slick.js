5ee4cc37a799f0ee3ee5e7d0d9022e5cee593350
lucastak <contato.lucas.santos@hotmail.com>
2022-12-26 23:29:43 -0300

    feat: adiciona estrutura do header e estiliza slick

--- pages/projetofinal/scripts/Slick.js
+++ pages/projetofinal/scripts/Slick.js
@@ -1,8 +1,11 @@
-$(".slider").slick({
+$(".slider__container").slick({
   dots: true,
   arrows: true,
   infinite: true,
   speed: 300,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  pauseOnHover: true,
   slidesToShow: 1,
   slidesToScroll: 1,
 });
